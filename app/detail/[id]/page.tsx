3a3ae5c6ae1fcf66c21f278143c7fb96321673b1
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-05-30 01:50:24 +0900

    🚩 Feat: delete functionality

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,20 +1,28 @@
 import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
-import Aside from '@/app/components/Aside';
-export default async function Detail(props) {
+import Aside from "@/app/components/Aside";
+import Button from "@/app/components/Button";
+import DeleteBtn from './DeleteBtn';
+import Link from "next/link";
+
+export default async function Detail(props:any) {
   const db = (await connectDB).db("forum");
   let result = await db
     .collection("post")
-    .findOne({ _id: new ObjectId(props.params.id) });
-
+    .findOne({ _id: new ObjectId(props.params.id) })
+  result._id = result._id.toString()
   return (
     <div>
-      <Aside/>
+      <Aside />
       <h4>datail</h4>
       <h4>{result.title}</h4>
       <p>{result.content}</p>
       <p>{result.category}</p>
-      <Aside banner={'banner'}/>
+      <Link href={`/edit/${result._id}`}>
+      <Button colorName={"bg-emerald-600"} buttonName={"수정"}/>
+      </Link> 
+      <DeleteBtn result={result}/>
+      <Aside banner={"banner"} />
     </div>
   );
 }
