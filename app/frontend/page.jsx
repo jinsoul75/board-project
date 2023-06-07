74d5a23d66642959a2277c1b319503c4672f04fb
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-06-08 01:09:18 +0900

    ✨ Feat: Create Router

---
+++ app/frontend/page.jsx
@@ -0,0 +1,28 @@
+import { connectDB } from "@/util/database";
+import ListItem from "../../components/ListItem";
+import Aside from "../../components/Aside";
+import tw from "tailwind-styled-components";
+
+export default async function Home() {
+  const db = (await connectDB).db("forum");
+  let result = await db.collection("post").find().toArray();
+  result = result.map((d) => {
+    d._id = d._id.toString();
+    return d;
+  });
+  const newResult = result.filter(d => d.category === 'FRONTEND')
+
+  return (
+    <Main>
+      <Aside />
+      <ListItem result={newResult} />
+      <Aside banner={'banner'} />
+    </Main>
+  );
+}
+
+const Main = tw.main`
+  flex
+  p-[20px]
+  overflow-auto
+`
