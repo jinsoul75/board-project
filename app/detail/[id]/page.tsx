80bc27ff7ffaddb3a6f376255d01d779a23b16bf
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-06-09 01:16:40 +0900

    ➕ Chore: Add Comment Component

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,27 +1,40 @@
 import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
 import Aside from "../../../components/Aside";
-import Button from "../../../components/Button";
 import DeleteBtn from "./DeleteBtn";
 import Link from "next/link";
-import tw from 'tailwind-styled-components';
+import tw from "tailwind-styled-components";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import Comment from "./Comment";
+
 export default async function Detail(props: any) {
+  const session: any = await getServerSession(authOptions);
   const db = (await connectDB).db("forum");
   let result = await db
     .collection("post")
     .findOne({ _id: new ObjectId(props.params.id) });
   result._id = result._id.toString();
+
   return (
     <Container>
       <Aside />
-      <h4>datail</h4>
-      <h4>{result.title}</h4>
-      <p>{result.content}</p>
-      <p>{result.category}</p>
-      <Link href={`/edit/${result._id}`}>
-        <Button colorName={"bg-emerald-600"} buttonName={"수정"} />
-      </Link>
-      <DeleteBtn result={result} />
+      <div className='flex flex-col p-[20px]'>
+        <h4>{result.title}</h4>
+        <p>{result.content}</p>
+        <p>{result.category}</p>
+        <Comment _id={result._id}/>
+        {session.user.email === result.email ? (
+          <>
+            <Link href={`/edit/${result._id}`}>
+              <button className='border border-soul-black rounded py-1 px-5'>
+                수정 버튼
+              </button>
+            </Link>
+            <DeleteBtn result={result} />
+          </>
+        ) : null}
+      </div>
       <Aside banner={"banner"} />
     </Container>
   );
@@ -29,4 +42,5 @@ export default async function Detail(props: any) {
 
 const Container = tw.div`
   flex
-`
+  p-[20px]
+`;
