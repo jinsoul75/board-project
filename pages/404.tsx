c94ee174127e8ff5a86c5b933ee26e0bf4a892d9
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-09-24 09:52:20 +0900

    Chore: 404,500 에러페이지

---
+++ pages/404.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import tw from 'tailwind-styled-components';
+import errorImg from '/public/images/404.jpg';
+
+export default function Custom404() {
+  return (
+    <Div>
+      <Image src={errorImg} alt="404Error" width={500} height={500} />
+      <h1 className="text-4xl font-bold">Oops!</h1>
+      <p className="text-xl">The page you're looking for doesn't exist.</p>
+    </Div>
+  );
+}
+
+const Div = tw.div`
+  flex
+`;
---
+++ pages/500.tsx
@@ -0,0 +1,8 @@
+export default function Custom500() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">500 - Server-side error occurred</h1>
+      <p className="text-lg">Sorry, something went wrong on our server.</p>
+    </div>
+  );
+}
