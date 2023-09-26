3a58fad2b4d4f94e67ed8ddae08f9bed69f4bdc8
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-09-27 02:51:15 +0900

    Design: 메인화면 구성변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,18 @@
-import tw from "tailwind-styled-components";
+import tw from 'tailwind-styled-components';
+import Image from 'next/image';
+import mainIng from '../public/images/main.jpg';
 
-import Aside from "../components/Aside";
+import Aside from '../components/Aside';
 
 export default async function Home() {
   return (
-    <>
-      <Main>
-        <Aside banner={null} />
-        <div>뭔가 멋진 메인 사진을 걸고 싶어요 캐러셀같은거 넣으면 좋겠다</div>
-        <Aside banner={"banner"} />
-      </Main>
-    </>
+    <div className="flex justify-between p-5">
+      <Aside/>
+      <main>
+        <Image src={mainIng} alt="main-image" height={300} width={500} />
+        <div> Hello World of Coding!</div>
+      </main>
+      <Aside banner={'banner'} />
+    </div>
   );
 }
-
-const Main = tw.main`
-  flex
-  justify-between
-  p-[20px]
-`;
