ffabeda80b92bdd1dbe6110488a2814eae41a592
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-09-27 15:02:44 +0900

    Chore: 메인화면 정렬

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,13 @@ import Aside from '../components/Aside';
 
 export default async function Home() {
   return (
-    <div className="flex justify-between p-5">
-      <Aside/>
-      <main>
+    <div className="flex justify-between p-5 mb-[40px]">
+      <Aside />
+      <main className="flex justify-center flex-col">
         <Image src={mainIng} alt="main-image" height={300} width={500} />
-        <div> Hello World of Coding!</div>
+        <div className='flex justify-center font-bold text-2xl'>
+          <div>Hello World of Coding!</div>
+        </div>
       </main>
       <Aside banner={'banner'} />
     </div>
