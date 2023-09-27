c882f80ce8aa60b6e0467ee61df9c04d6f761b2f
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-09-28 01:07:49 +0900

    Chore: 작업중

--- pages/api/comment/edit.tsx
+++ pages/api/comment/edit.tsx
@@ -6,7 +6,7 @@ import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const session: any = await getServerSession(req, res, authOptions);
-  console.log(req.body);
+
   if (req.method === 'POST') {
     if (req.body.comment === '') {
       return res.status(500).json('fill the title and content');
