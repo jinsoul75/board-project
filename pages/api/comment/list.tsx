9b57a140798aa8b53a6fb93f3bfa3d521a3d5a70
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-09-27 17:41:02 +0900

    Chore: 댓글 최신순 상단 정렬

--- pages/api/comment/list.tsx
+++ pages/api/comment/list.tsx
@@ -8,6 +8,6 @@ export default async function handler(
 ) {
   const id: string = req.query.id as string; 
   const db = (await connectDB).db("forum");
-  let result = await db.collection("comment").find({parent: new ObjectId(id)}).toArray();
+  let result = await db.collection("comment").find({parent: new ObjectId(id)}).sort({ date: -1 }).toArray();
   res.status(200).json(result);
 }
