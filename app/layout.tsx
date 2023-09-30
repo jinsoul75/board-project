c67ff2c3055cfa2081c93011e8778ffa1e42a6a7
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-10-01 01:00:39 +0900

    Fix: React is no defined. no-def

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -10,7 +11,7 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   return (
     <html lang='en'>
