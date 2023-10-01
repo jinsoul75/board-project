a10f973e52b5bd51c918c7f3fc11b38aed857a09
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-10-01 16:21:39 +0900

    Chore: database 타입 수정

--- util/database.tsx
+++ util/database.tsx
@@ -2,13 +2,20 @@ import { MongoClient } from 'mongodb';
 
 const url = process.env.DB_CONN_STRING as string;
 
-let connectDB:Promise<MongoClient>;
+let connectDB: Promise<MongoClient>;
+declare global {
+  // eslint-disable-next-line no-unused-vars
+  namespace globalThis {
+    // eslint-disable-next-line no-var, no-unused-vars
+    var _mongo: Promise<MongoClient>;
+  }
+}
 
 if (process.env.NODE_ENV === 'development') {
-  if (!global._mongo) {
-    global._mongo = new MongoClient(url).connect();
+  if (!globalThis._mongo) {
+    globalThis._mongo = new MongoClient(url).connect();
   }
-  connectDB = global._mongo;
+  connectDB = globalThis._mongo;
 } else {
   connectDB = new MongoClient(url).connect();
 }
