a5b44f1d27345b8552369c78ea5c51618d4d5162
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-10-01 18:54:12 +0900

    Chore: database 수정

--- util/database.tsx
+++ util/database.tsx
@@ -1,8 +1,10 @@
 import { MongoClient } from 'mongodb';
 
-const url = process.env.DB_CONN_STRING as string;
-
 let connectDB: Promise<MongoClient>;
+
+const mongo_username = process.env.MONGO_USERNAME;
+const mongo_password = process.env.MONGO_PASSWORD;
+
 declare global {
   // eslint-disable-next-line no-unused-vars
   namespace globalThis {
@@ -13,10 +15,10 @@ declare global {
 
 if (process.env.NODE_ENV === 'development') {
   if (!globalThis._mongo) {
-    globalThis._mongo = new MongoClient(url).connect();
+    globalThis._mongo = new MongoClient(`mongodb+srv://${mongo_username}:${mongo_password}@cluster0.vwcqeen.mongodb.net/?retryWrites=true&w=majority`).connect();
   }
   connectDB = globalThis._mongo;
 } else {
-  connectDB = new MongoClient(url).connect();
+  connectDB = new MongoClient(`mongodb+srv://${mongo_username}:${mongo_password}@cluster0.vwcqeen.mongodb.net/?retryWrites=true&w=majority`).connect();
 }
 export { connectDB };
