5b43605f34f5fdc69046b0b2572f3c8469ca1416
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-10-02 14:27:05 +0900

    Chore: 댓글 수정, 삭제시 바로 업데이트

--- components/comment/CommentItem.tsx
+++ components/comment/CommentItem.tsx
@@ -7,13 +7,17 @@ import tw from 'tailwind-styled-components';
 import { DataType } from '@/components/comment/Comment';
 import { UserInfo } from '@/app/detail/[id]/page';
 
-export default function CommentItem(props: { d: DataType; session: null | UserInfo }) {
+export default function CommentItem(props: {
+  d: DataType;
+  session: null | UserInfo;
+  fetchComments: () => void;
+}) {
   const [isEditing, setIsEditing] = useState(false);
   const [editedValue, setEditedValue] = useState(props.d.content);
 
   const editMode = isEditing ? '수정 완료' : '수정';
-  const onlyDate = props.d.date.split("T")[0]
-  const onlyTime = props.d.date.split("T")[1].slice(0,5)
+  const onlyDate = props.d.date.split('T')[0];
+  const onlyTime = props.d.date.split('T')[1].slice(0, 5);
 
   return (
     <div className="border my-4 p-4 flex justify-between">
@@ -31,49 +35,56 @@ export default function CommentItem(props: { d: DataType; session: null | UserIn
         <div className="text-sm text-slate-700">{onlyDate}</div>
         <div className="text-sm text-slate-700">{onlyTime}</div>
       </div>
-      {props.session && props.d.email === props.d.email ?
-      <div className="flex">
-        <Button
-          type="button"
-          className="mr-4"
-          onClick={(e) => {
-            e.preventDefault();
-            if (isEditing) {
-              if (props.d.content !== editedValue) {
+      {props.session && props.d.email === props.d.email ? (
+        <div className="flex">
+          <Button
+            type="button"
+            className="mr-4"
+            onClick={(e) => {
+              e.preventDefault();
+              if (isEditing) {
+                if (props.d.content !== editedValue) {
+                  axios
+                    .post('/api/comment/edit', {
+                      comment: editedValue,
+                      _id: props.d._id,
+                    })
+                    .then(() => {
+                      props.fetchComments();
+                    })
+                    .catch((error) => {
+                      console.error(error);
+                    });
+                }
+                setIsEditing(false);
+              } else {
+                setIsEditing(true);
+              }
+            }}
+          >
+            <AiOutlineEdit />
+            {editMode}
+          </Button>
+          <Button
+            type="button"
+            onClick={() => {
+              if (window.confirm('정말로 삭제하시겠습니까?')) {
                 axios
-                  .post('/api/comment/edit', {
-                    comment: editedValue,
+                  .post('/api/comment/delete', {
                     _id: props.d._id,
                   })
-                  .then(() => {})
-                  .catch((error) => {
-                    console.error(error);
-                  });
+                  .then(() => {
+                    props.fetchComments();
+                  })
+                  .catch((error) => console.error(error));
               }
-              setIsEditing(false);
-            } else {
-              setIsEditing(true);
-            }
-          }}
-        >
-          <AiOutlineEdit />
-          {editMode}
-        </Button>
-        <Button
-          type="button"
-          onClick={() => {
-            if (window.confirm('정말로 삭제하시겠습니까?')) {
-              axios.post('/api/comment/delete', {
-                _id: props.d._id,
-              });
-            }
-          }}
-        >
-          <AiOutlineDelete />
-          삭제
-        </Button>
-      </div>
-      : null}
+            }}
+          >
+            <AiOutlineDelete />
+            삭제
+          </Button>
+        </div>
+      ) : null}
     </div>
   );
 }
